5cc80331e6a03fbb0b73b3d0a65582b087b9f6eb
Els <Emathivannan@spartaglobal.com>
2018-09-27 11:28:28 +0100

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,24 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+package "nginx" do
+  action :install
+end
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  notifies(:restart, 'service[nginx]')
+end
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+  notifies(:restart, 'service[nginx]')
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+end
+
+service "nginx" do
+  action [:enable, :start]
+end
